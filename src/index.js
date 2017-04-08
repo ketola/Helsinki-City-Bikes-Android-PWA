0839be98491751d8aeaa5d45970546ca4ba5813d
Sauli Ketola <sauli.ketola@outlook.com>
2017-04-08 08:23:52 +0300

    Babel, ES6, ESLint, Flow, Jest, and Husky

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// @flow
+/* eslint-disable no-console */
+
+import Dog from './dog'
+
+const str = 'ES6'
+
+console.log(`Hello ${str}`)
+
+const toby = new Dog('Toby')
+
+console.log(toby.bark())
+
