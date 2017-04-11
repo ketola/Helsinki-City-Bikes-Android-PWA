6a9ddf4ec14f4cf9afa42b50c52f2a04fb239951
Sauli Ketola <sauli.ketola@nitor.fi>
2017-04-11 14:11:34 +0300

    list styles

--- src/client/component/bike-station.jsx
+++ src/client/component/bike-station.jsx
@@ -8,10 +8,14 @@ type Props = {
 }
 
 function Station(props) {
-  return <div>aaa{props.title}</div>
+  return  <div className='bike-station'>
+            <div className='title'>{props.station.get('title')}</div>
+            <div className='bikes'>Bikes: {props.station.get('bikesAvailable')} / {props.station.get('bikesTotal')}</div>
+            <div className='distance'>Distance: {props.station.get('distance')} m</div>
+          </div>
 }
 
 const BikeStation = ({ stations }: Props) =>
-  <div>{stations.map((st) => <Station title={st} />)}</div>
+  <div>{stations.map((st) => <Station station={st} />)}</div>
 
 export default BikeStation
