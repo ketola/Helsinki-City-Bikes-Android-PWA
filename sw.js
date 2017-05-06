43053afc4d5ebce5173d35b9aa4bd1e4d5fe323b
Sauli Ketola <sauli.ketola@nitor.fi>
2017-05-06 18:50:27 +0300

    Support versioning the application and clean old caches when service worker is installed

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-console.log('sw');
-
-var CACHE_NAME = 'hcb-cache1';
+var APP_VERSION = '0.0.7'
+var CACHE_NAME = 'hcb-cache_' + APP_VERSION
 var urlsToCache = [
   '/',
   '/index.html',
   '/public/css/style.css',
-  '/js/bundle.js',
-  'https://api.digitransit.fi/routing/v1/routers/hsl/bike_rental'
+  '/js/bundle.js'
 ];
 
 self.addEventListener('activate', function(event) {
-  console.error('activate');
+  console.log('activate service worker');
+  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (CACHE_NAME !== key) {
+          console.log('delete old cache ' + key)
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 self.addEventListener('install', function(event) {
-  console.log('install');
+  console.log('install service worker');
+  event.waitUntil(self.skipWaiting());
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
